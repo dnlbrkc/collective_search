fc1723184cb505999ae50bafc46a4f0f57eee4d5
Daniel B <dnlbrkc@gmail.com>
2016-04-25 16:13:50 +0200

    file for combining results

--- analysis/combine_results.R
+++ analysis/combine_results.R
@@ -1,4 +1,4 @@
-setwd("~/Desktop/ResultsApr22")
+setwd("~/Desktop/res")
 
 files <- list.files()
 
@@ -15,3 +15,14 @@ total<- total + output
 total <- total/length(files)
 
 # save(total,file="~/Desktop/Results.Rdata")
+
+par(mfrow=c(5,3),mar=c(1,1,1,1))
+for(i in 1:15){
+plot(total[,i,2],type='l',col="blue",ylim=c(0.5,1))
+lines(total[,i,3],type='l')
+
+lines(total[,i,4],type='l',col="blue")
+lines(total[,i,5],type='l')
+  
+}
+
