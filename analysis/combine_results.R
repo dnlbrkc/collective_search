501b3c96d85a418227514ffc8bfc4c0c962014ee
Charley Wu <charleymswu@gmail.com>
2016-04-29 19:28:32 +0200

    new analysis

--- analysis/combine_results.R
+++ analysis/combine_results.R
@@ -1,28 +1,16 @@
-setwd("~/Desktop/res")
+folder<- "resultsApr29"
 
+setwd(folder)
 files <- list.files()
-
-total <- array(0,dim=c(100,16,7))
-
+total <- array(0,dim=c(100,17,7))
 for(f in files){
-
-load(f)
-
-total<- total + output
-
+	load(f)
+	total<- total + output
 }
 
 total <- total/length(files)
 
-# save(total,file="~/Desktop/Results.Rdata")
-
-par(mfrow=c(5,3),mar=c(1,1,1,1))
-for(i in 1:15){
-plot(total[,i,2],type='l',col="blue",ylim=c(0.5,1))
-lines(total[,i,3],type='l')
-
-lines(total[,i,4],type='l',col="blue")
-lines(total[,i,5],type='l')
-  
-}
+setwd("..")
+filename <- paste0(folder, ".Rdata", sep="")
+save(total,file=filename)
 
